887604d0139425c5bf29ebddc0b8da22f545370d
AlbertoVosgerau <36092283+AlbertoVosgerau@users.noreply.github.com>
2020-11-15 23:30:15 -0300

    Small updates

--- Assets/UnityXRUtilities/Scripts/Interactions/XRButtonInteractor.cs
+++ Assets/UnityXRUtilities/Scripts/Interactions/XRButtonInteractor.cs
@@ -7,8 +7,18 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRButtonInteractor : MonoBehaviour
 {
+    [SerializeField] private Transform buttonMesh;
+    [SerializeField] private Vector3 onPressedLocalPosition;
+
+    private Vector3 originalLocalPosition;
+
     public UnityEvent onButtonPressed;
     public UnityEvent onButtonReleased;
+
+    private void Awake()
+    {
+        originalLocalPosition = buttonMesh.transform.localPosition;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +26,11 @@ public class XRButtonInteractor : MonoBehaviour
             return;
 
         onButtonPressed.Invoke();
+
+        if (buttonMesh == null)
+            return;
+
+        StartCoroutine(UpdateTransform(onPressedLocalPosition));
     }
     private void OnTriggerExit(Collider other)
     {
@@ -23,5 +38,25 @@ public class XRButtonInteractor : MonoBehaviour
             return;
 
         onButtonReleased.Invoke();
+
+        if (buttonMesh == null)
+            return;
+
+        StartCoroutine(UpdateTransform(originalLocalPosition));
+    }
+
+    private IEnumerator UpdateTransform(Vector3 targetPosition)
+    {
+        float time = 0;
+        Debug.Log("Time zero");
+        do
+        {
+            buttonMesh.localPosition = Vector3.Lerp(buttonMesh.localPosition, targetPosition, time / 0.5f);
+            Debug.Log($"Pos: {buttonMesh.localPosition}");
+            time += Time.deltaTime;
+            Debug.Log("Time");
+            yield return null;
+        }
+        while (time < 0.5f);
     }
 }
