141e10ff81e6ab5b5c0cdc4344a4fce334946fa0
AlbertoVosgerau <36092283+AlbertoVosgerau@users.noreply.github.com>
2020-11-17 20:03:48 -0300

    Scroll

---
+++ Assets/UnityXRUtilities/Scripts/UI/XRScrollRectAxisInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class XRScrollRectAxisInput : MonoBehaviour
+{
+    private ScrollRect scrollRect;
+
+    private void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>(); 
+    }
+
+    private void Update()
+    {
+        XRInputDevices.RightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 test);
+
+        Scroll(test);
+    }
+
+    public void Scroll(Vector2 scrollValue)
+    {
+        scrollValue = scrollValue / 10;
+
+        if (scrollRect == null)
+            return;
+
+        if (scrollRect.horizontal)
+        {
+            scrollRect.verticalNormalizedPosition += scrollValue.y;
+        }
+
+        if (scrollRect.vertical)
+        {
+            scrollRect.verticalNormalizedPosition += scrollValue.y;
+        }
+    }
+}
