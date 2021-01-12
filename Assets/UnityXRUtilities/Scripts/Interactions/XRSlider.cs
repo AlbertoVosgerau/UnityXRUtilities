5dc4c34c02faed3eb151bd5b8a73bdc9222e36db
Camilla "Nanie" Camatta <camillacamatta@gmail.com>
2021-01-11 22:30:13 -0300

    New XR Slider

---
+++ Assets/UnityXRUtilities/Scripts/Interactions/XRSlider.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRSlider : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private Transform _handler;
+    [SerializeField] private Transform _upLimit;
+    [SerializeField] private Transform _downLimit;
+    [SerializeField] private MyFloatEvent _onValueChange;
+    [SerializeField] private MyFloatEvent _onValueChangeEnd;
+
+    private float _currentValue;
+    private float _maxDistance;
+    private void Start()
+    {
+        _maxDistance = Vector3.Distance(_downLimit.position, _upLimit.position);
+        _currentValue = GetCurrentValue();
+
+        if (_handler.gameObject.TryGetComponent(out XRBaseInteractable interactable))
+        {
+            interactable.onDeactivate.AddListener(EndGrab);
+        }
+
+        if (_text != null)
+            _text.text = (_currentValue * 100).ToString("N0");
+    }
+
+    private void EndGrab(XRBaseInteractor interactor)
+    {
+        _onValueChangeEnd.Invoke(_currentValue);
+    }
+
+    void Update()
+    {
+        _handler.position = VectorUtils.ClampPoint(_handler.position, _upLimit.position, _downLimit.position);
+        var newValue = GetCurrentValue();
+        if (_currentValue != newValue)
+        {
+            _currentValue = newValue;
+            _onValueChange.Invoke(_currentValue);
+            if (_text != null)
+                _text.text = (_currentValue * 100).ToString("N0");
+        }
+    }
+
+    private float GetCurrentValue()
+    {
+        return Vector3.Distance(_downLimit.position, _handler.position) / _maxDistance;
+    }
+}
+
+[System.Serializable]
+public class MyFloatEvent : UnityEvent<float>
+{
+}
---
+++ Assets/UnityXRUtilities/Scripts/Utils/VectorUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Original reference - https://answers.unity.com/questions/1229112/clamp-a-vector3-position-to-an-arbitrary-line-segm.html
+public class VectorUtils 
+{
+    public static Vector3 ClampPoint(Vector3 point, Vector3 segmentStart, Vector3 segmentEnd)
+    {
+        return ClampProjection(ProjectPoint(point, segmentStart, segmentEnd), segmentStart, segmentEnd);
+    }
+
+    public static Vector3 ProjectPoint(Vector3 point, Vector3 segmentStart, Vector3 segmentEnd)
+    {
+        return segmentStart + Vector3.Project(point - segmentStart, segmentEnd - segmentStart);
+    }
+
+    private static Vector3 ClampProjection(Vector3 point, Vector3 start, Vector3 end)
+    {
+        var toStart = (point - start).sqrMagnitude;
+        var toEnd = (point - end).sqrMagnitude;
+        var segment = (start - end).sqrMagnitude;
+        if (toStart > segment || toEnd > segment) return toStart > toEnd ? end : start;
+        return point;
+    }
+}
